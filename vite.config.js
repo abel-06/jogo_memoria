efe60c427c6ecfcdaf803517057faab021b781b4
Abeljoga10 <narutados123@gmail.com>
2025-06-24 11:19:21 -0300

    testando

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dia_namorados",
+  base: "/jogo_memoria/",
 })
