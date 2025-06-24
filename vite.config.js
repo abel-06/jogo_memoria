7d337e146fd767076069e65be447f73dbdc0b324
Abeljoga10 <narutados123@gmail.com>
2025-06-24 15:54:37 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./jogo_memoria/",
+  base: "/jogo_memoria/",
 })
